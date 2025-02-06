9ac7c9b1115f7c6bb4199a5aecd610c3966f2ead
JMDETETH <kevintr1005@gmail.com>
2025-02-05 20:11:46 -0800

    added playing options

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,16 +1,20 @@
 import Game from "../components/Game.jsx";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import "../styles/GamePage.css";
 
 function GamePage() {
     const navigate = useNavigate();
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const nBalls = params.get("numberOfBalls");
+    const nSeconds = params.get("maxSeconds");
     function handleExit() {
         navigate("/");
     }
     return (
         <div className="page" style={{backgroundColor: '#fffbe2'}}>
             <button className="button" onClick={handleExit} style={{width:'10px', position: 'absolute'}}>X</button>
-            <Game />
+            <Game numberOfballs={nBalls} maxSeconds={nSeconds} />
         </div> 
     )
 }
