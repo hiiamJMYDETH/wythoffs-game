a8c23bec6b2a98750f7ebef39caacd9b2d2a91a3
JMDETETH <kevintr1005@gmail.com>
2025-02-19 13:21:27 -0800

    Added other components from main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,25 @@
-import { useState, useEffect } from 'react'
-import {fetching} from './components/utilities.jsx';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useMobileDetect } from "./components/utilities.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import HomePageM from "./pages/mobile/HomePage.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isMobile = useMobileDetect();
+  const location = useLocation();
 
-  useEffect(() => {
-    fetching();
-  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isMobile ? (
+        <Routes>
+          <Route path="/" element={<HomePageM />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      )}
     </>
   )
 }
