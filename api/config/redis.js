dd61ef195a701b5997c130fa9749fc3a7b9df0ea
Kevin Trinh <kevintr1005@gmail.com>
2025-08-24 18:35:17 -0700

    Removed some debugging messages

--- api/config/redis.js
+++ api/config/redis.js
@@ -8,7 +8,6 @@ const redisClient = createClient({
 });
 
 redisClient.on('error', (err) => console.error('❌ Redis Error:', err));
-redisClient.on('connect', () => console.log('✅ Connected to Redis'));
 
 await redisClient.connect(); 
 
