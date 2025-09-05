1f7d4c573b6fe453f85407e9fa887aa1ae8399cd
Kevin Trinh <kevintr1005@gmail.com>
2025-09-04 22:01:17 -0700

    Revised player profile

--- frontend/src/components/Player.jsx
+++ frontend/src/components/Player.jsx
@@ -1,39 +1,65 @@
 import "../styles/page.css";
 import UserDefault from "../assets/User default.svg";
 import { fetching } from "./utilities";
+import { useEffect, useState } from "react";
 import CPU from "../assets/CPU.svg";
 
 function Player({ name }) {
-    const token = localStorage.getItem('token');
-    if (token && name != 'computer') {
-        loadUsers();
-    }
-    async function loadUsers() {
-        try {
-            const users = await fetching('users');
-            console.log('Users:', users);
-            if (users[0].username != name) {
-                console.error("Error matching username");
-                return;
+    const [loading, setLoading] = useState(false);
+    const [win, setWin] = useState(0);
+    const [loss, setLoss] = useState(0);
+    const [username, setUsername] = useState(null);
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        if (name != 'computer') {
+            async function loadUsers() {
+                try {
+                    setLoading(true);
+                    const users = await fetching('users');
+                    if (users) {
+                        const matchedUser = users.find(user => user.id === name);
+                        console.log("User: ", matchedUser);
+                        if (matchedUser) {
+                            const { username, win, lose } = matchedUser;
+                            console.log("Username: ", username);
+                            setUser(matchedUser);
+                            setUsername(username);
+                            setWin(win);
+                            setLoss(lose);
+                        }
+                    }
+                } catch (error) {
+                    console.error('Error loading users:', error.message);
+                    localStorage.removeItem('token');
+                } finally {
+                    setLoading(false);
+                }
             }
-        } catch (error) {
-            console.error('Error loading users:', error.message);
-            localStorage.removeItem('token');
+            loadUsers();
         }
-    }
+    }, [name]);
 
     var image = UserDefault;
     if (name === "computer") {
         image = CPU;
+        setUsername("CPU")
     }
     return (
-        <div style={{ display: 'flex' }}>
-            <img src={image} />
-            <div>
-                <h2>{name}</h2>
-                <p>Win-Loss: 0-0</p>
-            </div>
-        </div>
+        <>
+            {loading ? (
+                <h1>...</h1>
+            ) :
+                (
+                    <div style={{ display: 'flex' }}>
+                        <img src={image} />
+                        <div>
+                            <h2>{username}</h2>
+                            <p>Win-Loss: {win}-{loss}</p>
+                        </div>
+                    </div>
+                )
+            }
+        </>
     )
 }
 
